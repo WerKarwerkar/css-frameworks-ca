044c4e84c40d0ac500f0e5f3dc83e856d16c506b
WerKarwerkar <126430261+WerKarwerkar@users.noreply.github.com>
2024-02-23 16:26:35 +0100

    js

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,5 +1,6 @@
 import { setRegisterFormListener } from "./handlers/register.mjs";
 import { setLoginFormListener } from "./handlers/login.mjs";
+import * as post from "./api/posts/index.mjs";
 
 
 const path = location.pathname;
@@ -11,3 +12,42 @@ if (path === '/profile/login/index.html') {
 }
 
 
+// post.createPost()
+// post.updatePost()
+// post.removePost()
+// post.getPost()
+// post.getPosts().then(console.log)
+// post.getPost(10596).then(console.log)
+
+// removePost(10589) 
+
+// post.createPost()
+// post.updatePost()
+// post.removePost()
+// post.getPost()
+// post.getPosts()
+
+
+
+// title: "hello, test post", body: "another test :)", tags: [], media: null,…}
+// body
+// : 
+// "another test :)"
+// created
+// : 
+// "2024-02-22T13:53:00.173Z"
+// id
+// : 
+// 10589
+// media
+// : 
+// null
+// tags
+// : 
+// []
+// title
+// : 
+// "hello, test post"
+// updated
+// : 
+// "2024-02-22T13:53:00.173Z"
